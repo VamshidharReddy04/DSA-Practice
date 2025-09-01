247568c5c9f8e0fc229e714fb2accb26f54aac7f
VamshidharReddy04 <nvamshidharreddy3@gmail.com>
2025-09-01 14:34:27 +0530

    initial commit

--- Day01-Two-sum/two-sum.py
+++ Day01-Two-sum/two-sum.py
@@ -6,3 +6,21 @@ def twoSums(nums, target):
             return [seen[result], i]
         seen[num] = i
 print(twoSums([2, 7, 11, 15], 9))
+
+# time complexity: O(n)
+# space complexity: O(n)
+
+# what if the array is sorted
+def twoSumSorted(nums,target):
+    left ,right = 0 , len(nums)-1
+    while left < right:
+        s=nums[left]+nums[right]
+        if s==target:       #In python == is used to check equality but 
+                            #in javascript == is used compares values after type Conversionso we have to use ===(strictly equal) compares both value and type
+            return[left,right]
+        else:
+            if s<target:
+                left+=1
+            else:
+                right-=1
+print(twoSumSorted([2,7,11,15],9))
--- Day01-Two-sum/two_sum.js
+++ Day01-Two-sum/two_sum.js
@@ -9,3 +9,28 @@ function twoSum(nums,target){
     }
 }
 console.log(twoSum([2,7,11,15],9))
+// time complexity O(n)
+// space complexity O(n)
+
+// what if the array is sorted
+
+function twoSumSorted(nums,target){
+    let left =0 ,right=nums.length-1;
+    while(left<right){
+        let s=nums[left]+nums[right];
+        if(s === target){            // In python == is used to check equality but 
+                                     //in javascript ==(loose equality) is used compares values after type Conversionso 
+                                    // we have to use ===(strictly equal) compares both value and type
+            return[left,right];
+        }else if(s<target){
+            left++;
+        }else{
+            right--;
+        }
+    }return[]
+}
+console.log(twoSumSorted([2,7,11,15],9))
+
+// time complexity O(n)
+// space complexity O(1)
+
